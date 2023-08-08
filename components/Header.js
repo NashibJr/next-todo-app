f21cc67cea95fadaa90bdfb8c33e31aeb5cdadd8
NashibJr <nashibjr@gmail.com>
2023-08-08 04:56:45 -0700

    Finished the header component

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-center items-center bg-blue-800 text-white w-full p-4">
+      <h1>Todo App</h1>
+    </div>
+  );
+};
+
+export default Header;
