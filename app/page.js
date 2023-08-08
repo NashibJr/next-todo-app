adcf9174806ca69b2932f123d8e6ff76288eb89b
NashibJr <nashibjr@gmail.com>
2023-08-08 08:30:22 -0700

    Changed the todos to be fetched from the page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,18 @@
+"use client";
+
 import Header from "@/components/Header";
 import Todos from "@/components/Todos";
 import Form from "@/components/form";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const state = useSelector((state) => state.todos);
+  const todos = state?.todos;
   return (
     <div>
       <Header />
       <Form />
-      <Todos />
+      <Todos todos={todos} />
     </div>
   );
 };
