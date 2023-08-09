c812c995b1ec9389ba58ad53ae781f6e0ae8a83f
NashibJr <nashibjr@gmail.com>
2023-08-09 07:34:53 -0700

    Stylesd the model

---
+++ components/Modal.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+export default function Modal() {
+  const [showModal, setShowModal] = React.useState(false);
+  return (
+    <>
+      <button
+        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Open regular modal
+      </button>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mx-auto my-auto w-full">
+            <div className="relative my-6 mx-auto max-w-3xl sm:w-2/4 xl:w-2/5 w-auto">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-3 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-2xl font-semibold">Edit task</h3>
+                </div>
+                {/*body*/}
+                <div className="relative p-3 flex-auto">
+                  <input
+                    type="text"
+                    placeholder="Edit task from here..."
+                    className="border border-b-slate-200 rounded-sm p-2 outline-none text-sm w-full"
+                  />
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-3 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-2 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-2 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
--- components/Todos.js
+++ components/Todos.js
@@ -1,9 +1,13 @@
+import Modal from "./Modal";
 import Todo from "./Todo";
 
 const Todos = ({ todos }) => {
   return (
     <div className="m-2 mt-6 md:w-3/4 md:mx-auto lg:w-2/3 p-4 xl:w-1/2">
       {todos && todos.map((todo) => <Todo key={todo.id} todo={todo} />)}
+      <div>
+        <Modal />
+      </div>
     </div>
   );
 };
